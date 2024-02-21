9332bf1a7c655cad7b646b8f2fa278c71647679e
Peter Vu <dmtruong1980@gmail.com>
2024-02-20 19:52:37 -0600

    feat: Add a function to convert word input
    Add a function that converts the option "Rock", "Paper", "Scissors" or
    any other string into a numeric code from 0 to 2 respectively, or -1.

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-function getComputerChoice() {
-    const ROCK_CODE = 0;
-    const SCISSORS_CODE = 1;
-    const PAPER_CODE = 2;
+const ROCK_CODE = 0;
+const SCISSORS_CODE = 1;
+const PAPER_CODE = 2;
 
+const ROCK = "Rock";
+const SCISSORS = "Scissors";
+const PAPER = "Paper";
+
+const INVALID = -1;
+
+function getComputerChoice() {
     const computerChoiceCode = Math.floor(Math.random() * 3);
 
     let computerChoice;
 
     switch(computerChoiceCode) {
         case ROCK_CODE:
-            computerChoice = "Rock";
+            computerChoice = ROCK;
             break;
         
         case SCISSORS_CODE:
-            computerChoice = "Scissors";
+            computerChoice = SCISSORS;
             break;
 
         case PAPER_CODE:
-            computerChoice = "Paper";
+            computerChoice = PAPER;
             break;
 
         default:
@@ -25,4 +31,28 @@ function getComputerChoice() {
     }
 
     return computerChoice;
+}
+
+function getCode(selection) {
+    let result;
+
+    switch (selection.toLowerCase()) {
+        case ROCK.toLowerCase():
+            result = ROCK_CODE;
+            break;
+        
+        case SCISSORS.toLowerCase():
+            result = SCISSORS_CODE;
+            break;
+        
+        case PAPER.toLowerCase():
+            result = PAPER_CODE;
+            break;
+        
+        default:
+            result = INVALID;
+            break;
+    }
+
+    return result;
 }
