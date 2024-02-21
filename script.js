2ab28eaa822c4565d2fd785c90b966efdc21b0c1
Peter Vu <dmtruong1980@gmail.com>
2024-02-20 20:14:52 -0600

    feat: Refactor and add functions to play a round
    - Move around some constants so they can be accessed by all functions.
    Also change assignments to be of said constants.
    - Revamp the getCode() function to compare directly in the switch,
    without converting the parameter.
    - Move said conversion to the new function, playRound().
    - Add the function playRound() which takes two parameters, convert them
    into codes and do arithmetic comparisons to print out the results.
    Unnecessarily complicated over just doing nested if/switch statements,
    but it was funny!

--- script.js
+++ script.js
@@ -36,22 +36,63 @@ function getComputerChoice() {
 function getCode(selection) {
     let result;
 
-    switch (selection.toLowerCase()) {
-        case ROCK.toLowerCase():
+    switch (selection) {
+        case ROCK:
             result = ROCK_CODE;
             break;
         
-        case SCISSORS.toLowerCase():
+        case SCISSORS:
             result = SCISSORS_CODE;
             break;
         
-        case PAPER.toLowerCase():
+        case PAPER:
             result = PAPER_CODE;
             break;
         
         default:
             result = INVALID;
             break;
+    }
+
+    return result;
+}
+
+function playRound(playerSelection, computerSelection) {
+    let result;
+
+    playerSelection = playerSelection.length > 2 ? 
+        playerSelection.at(0).toUpperCase() + playerSelection.slice(1).toLowerCase() :
+        INVALID;
+
+    const playerSelectionCode = getCode(playerSelection);
+    const computerSelectionCode = getCode(computerSelection);
+
+    const comparisonCode = playerSelectionCode - computerSelectionCode;
+
+    /*
+        rock = 0
+        scissors = 1
+        paper = 2
+        
+        rock - rock = 0 => draw
+        rock - scissors = -1 => win
+        rock - paper = -2 => lose
+        scissors - rock = 1 => lose
+        scissors - scissors = 0 => draw
+        scissors - paper = -1 => win
+        paper - rock = 2 => win
+        paper - scissors = 1 => lose
+        paper - paper = 0 => draw
+    */
+    
+    if (playerSelectionCode === INVALID) {
+        result = "Invalid selection";
+    } else if (comparisonCode === 0) {
+        result = `You Draw! ${playerSelection} is the same as ${computerSelection}`;
+    } else if (comparisonCode === -1 || comparisonCode === 2) {
+        result = `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        result = `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
 
     return result;
